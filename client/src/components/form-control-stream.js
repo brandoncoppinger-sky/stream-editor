b026a39a6ec4d528ad6541106fa35fb3bc8e8336
Brandon Coppinger <brandon.coppinger@sky.uk>
2020-12-06 12:44:21 +0000

    testing of embedded child object

---
+++ client/src/components/form-control-stream.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+
+export const FormControlStream = ({streamValues}) => {
+
+
+    // return (
+
+    // )
+};
--- client/src/components/stream-details.js
+++ client/src/components/stream-details.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, OverlayTrigger, ButtonToolbar, Tooltip } from 'react-bootstrap'
+import { Formik } from 'formik'
+import { Button, OverlayTrigger, ButtonToolbar, Tooltip, Form } from 'react-bootstrap'
 
 class StreamDetails extends React.Component {
 
@@ -18,39 +19,55 @@ class StreamDetails extends React.Component {
       .catch(error => console.log(error));
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(e)
+  }
+
+  onReset(e) {
+    e.preventDefault();
+  }
+
   render() {
     const keys = Object.keys(this.state.stream).map((keys) => (keys))
     return (
-      <form className="stream-form">
-        {keys.map((key, index) => {
-          return (
-            <div className="form-group" key={index}>
-              <label key={`${index}-label`}>{key}</label>
-              {
-                <div>
-                  <input className="form-control" id="stream-inputs" key={`${index}-input`} defaultValue={this.state.stream[key]} />
-                  <ButtonToolbar id="stream-tooltip">
-                    {['right'].map(placement => (
-                      <OverlayTrigger
-                        key={placement}
-                        placement={placement}
-                        overlay={
-                          <Tooltip id={`tooltip-${placement}`}>
-                            <strong>Please enter the correct details for {key}</strong>.
+      <Formik>
+        <form className="stream-form" onSubmit={this.handleSubmit} onReset={this.handleReset}>
+          {keys.map((key, index) => {
+            return (
+              <div className="form-group" key={index}>
+                <Form.Group>
+
+                <label key={`${index}-label`}>{key}</label>
+                {
+                  <div>
+                    {typeof this.state.stream[key] == 'object' && console.log(this.state.stream[key])}
+                    <Form.Control id="stream-inputs" key={`${index}-input`} defaultValue={this.state.stream[key]} />
+                    <ButtonToolbar id="stream-tooltip">
+                      {['right'].map(placement => (
+                        <OverlayTrigger
+                          key={placement}
+                          placement={placement}
+                          overlay={
+                            <Tooltip id={`tooltip-${placement}`}>
+                              <strong>Please enter the correct details for {key}</strong>.
                           </Tooltip>
-                        }
-                      >
-                        <Button variant="success" className="ml-1">i</Button>
-                      </OverlayTrigger>
-                    ))}
-                  </ButtonToolbar>
-                </div>
-              }
-            </div>
-          )
-        })}
-        <Button as="input" type="submit" value="Save" />
-      </form>
+                          }
+                        >
+                          <Button variant="success" className="ml-1">i</Button>
+                        </OverlayTrigger>
+                      ))}
+                    </ButtonToolbar>
+                  </div>
+                }
+                </Form.Group>
+              </div>
+            )
+          })}
+          <Button as="input" type="submit" value="Save" />
+        </form>
+      </Formik>
+
     )
   }
 };
